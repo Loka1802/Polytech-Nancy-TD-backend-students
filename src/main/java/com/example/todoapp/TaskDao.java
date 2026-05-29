5755f70b2a0d043a01542753bc2b32ff0fffa251
Lucie OUCH <ouchlucie@gmail.com>
2026-05-29 17:46:04 +0200

    TD pas fini

--- src/main/java/com/example/todoapp/TaskDao.java
+++ src/main/java/com/example/todoapp/TaskDao.java
@@ -1,38 +1,142 @@
 package com.example.todoapp;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.*;
+import java.util.*;
 
 /**
  * Data Access Object for {@link Task} model.
  */
 public class TaskDao {
 
-    private final Map<Integer, Task> storage = new HashMap<>();
+    private static final String DB_URL = "jdbc:sqlite:database.db";
+    private static final Logger log = LoggerFactory.getLogger(TaskDao.class);
 
-    {
+    public TaskDao() {
+        try {
+            createTableIfNotExists();
+            initializeTable();
+        } catch (SQLException e) {
+            log.error("Error while initializing table", e);
+        }
+    }
+
+    private void createTableIfNotExists() throws SQLException {
+        String sql = """
+                        CREATE TABLE IF NOT EXISTS Tasks (
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        title TEXT NOT NULL,
+                        description TEXT NOT NULL,
+                        done BOOLEAN NOT NULL 
+                    );
+                """;
+
+        try (Connection conn = DriverManager.getConnection(DB_URL);
+             Statement stmt = conn.createStatement()) {
+            stmt.execute(sql);
+        }
+    }
+
+    public void initializeTable() throws SQLException {
         save(new Task(1, "Réviser DS de maths", "Séries numériques et probabilités.", false));
         save(new Task(2, "Valider mon PIVE", "PIVE Club Poker.", true));
         save(new Task(3, "Choisir mon parcours de 4A", "SIR ou SIA ?", false));
     }
 
+    private static Task buildTaskModel(ResultSet rs) throws SQLException {
+        return new Task(rs.getInt("id"), rs.getString("title"), rs.getString("description"), rs.getBoolean("done"));
+    }
+
     /**
      * Persist {@link Task} model.
+     *
      * @param task task to save.
      * @return task model.
      */
-    public Task save(Task task) {
-        storage.put(task.id(), task);
-        return task;
+    public Task save(Task task) throws SQLException {
+        String sql = "INSERT INTO Tasks (title, description, done) VALUES (?, ?, ?) RETURNING id, title, description, done;";
+
+        try (Connection conn = DriverManager.getConnection(DB_URL);
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setString(1, task.title());
+            ps.setString(2, task.description());
+            ps.setBoolean(3, task.done());
+            ResultSet rs = ps.executeQuery();
+
+            return buildTaskModel(rs);
+        } catch (SQLException e) {
+            throw new RuntimeException("failed", e);
+        }
+
     }
 
     /**
      * Retrieve {@link Task} model by id.
+     *
      * @param id identifier of the {@link Task}.
      * @return {@link Task} model wrapped by Optional.
      */
-    public Optional<Task> findById(int id) {
-        return Optional.ofNullable(storage.get(id));
+    public Optional<Task> findById(int id) throws SQLException {
+        String sql = "SELECT id, title, description, done FROM Tasks WHERE id = ?;";
+
+        try (Connection conn = DriverManager.getConnection(DB_URL);
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setLong(1, id);
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    return Optional.of(buildTaskModel(rs));
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Task retrieval failed", e);
+        }
+        return Optional.empty();
+    }
+
+    /**
+     * Retrieve all {@link Task} models.
+     * @return list of tasks.
+     */
+    public List<Task> findAll() {
+        String sql = "SELECT id, title, description, done FROM Tasks;";
+
+        try (ResultSet rs = ps.executeQuery()) {
+            if (rs.next()) {
+                return Optional.of(buildTaskModel(rs));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Task retrieval failed", e);
+        }
+        return Optional.empty();
+        return new ArrayList<>(storage.values());
     }
 }
+/*
+    /**
+     * Delete {@link Task} model by id.
+     * @param id identifier of the {@link Task}.
+     * @return true if task was deleted, false otherwise.
+     *
+    public boolean delete(int id) {
+        return storage.remove(id) != null;
+    }
+
+    /**
+     * Update {@link Task} model.
+     * @param task updated task.
+     * @return updated {@link Task} model wrapped by Optional.
+     *
+    public Optional<Task> update(Task task) {
+
+        if (!storage.containsKey(task.id())) {
+            return Optional.empty();
+        }
+
+        storage.put(task.id(), task);
+
+        return Optional.of(task);
+    }
+}
+*/
